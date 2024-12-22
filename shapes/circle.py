925fd0b4ff486c19f16b6e492c1ae388f7490cd4
Landon Prince <landoncprince@gmail.com>
2024-12-22 01:31:33 -0600

    v2

--- shapes/circle.py
+++
@@ -1,2 +0,0 @@
-pygame.draw.circle(screen, "GREEN", (100, 105), 48)  
-pygame.draw.circle(screen, "black", (100, 105), 48, 5) 
--- shapes/diamond.py
+++
@@ -1,9 +0,0 @@
-base_x, base_y = 480, 230  
-diamond_points = [
-    (base_x, base_y - 50),  # Top
-    (base_x + 50, base_y),  # Right
-    (base_x, base_y + 50),  # Bottom
-    (base_x - 50, base_y)   # Left
-]
-pygame.draw.polygon(screen, "GREEN", diamond_points)  # Filled
-pygame.draw.polygon(screen, "BLACK", diamond_points, 7)  # Outline
