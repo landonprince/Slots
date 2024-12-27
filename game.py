f78cb11c351d417825ec4f7154efecd37613f5ec
Landon Prince <landoncprince@gmail.com>
2024-12-27 17:01:08 -0600

    v10

--- game.py
+++ game.py
@@ -3,20 +3,25 @@ from sys import exit
 import random
 import shapes
 
+# pygame initialization
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 pygame.display.set_caption("Slots")
 clock = pygame.time.Clock() 
 
+# return a list of 16 shapes of random type and color
 def generate_shapes():
-    points_copy = shapes.points[:]
+    points_copy = shapes.points[:] # create copy since we will be popping points
     shape_list = []
+    
+    # place random shapes at random points in the 4x4 grid
     while points_copy:
         index = random.randint(0, len(points_copy) - 1)
         x, y = points_copy.pop(index)
         
         shape_type, color = shapes.get_random_shape()
         
+        # make specific shape object based on type and add to shape list
         if shape_type == "square":
             shape = shapes.Square(color, x, y)
         elif shape_type == "circle":
@@ -32,9 +37,13 @@ def generate_shapes():
         
     return shape_list
 
+all_shapes = generate_shapes() # initialize list of random shapes
+
+# dimensions of the outer background & inner background
 outer_bg_x, outer_bg_y = 750, 550
 inner_bg_x, inner_bg_y = 720, 520
 
+# initialize rects for the outer, inner, & side backgrounds
 outer_bg = pygame.Rect((screen.get_width() - outer_bg_x) // 2, 
                        (screen.get_height() - outer_bg_y) // 2, 
                         outer_bg_x, outer_bg_y)
@@ -45,72 +54,100 @@ side_bg = pygame.Rect((screen.get_width() - inner_bg_x) // 2 + 510,
                       (screen.get_height() - inner_bg_y) // 2, 
                        inner_bg_x - 510, inner_bg_y)
 
+# draw the game background
 def draw_background():
-    screen.fill("brown4")
+    screen.fill("brown4") # fill the entire screen (red-brown)
     
+    # draw outer background (dark-brown)
     pygame.draw.rect(screen, (100, 70, 40), outer_bg, 20, 15)
     pygame.draw.rect(screen, "black", outer_bg, 5, 15)
 
-    pygame.draw.rect(screen, "bisque4", inner_bg, 0, 10)
+    # draw inner background (gray-brown)
+    pygame.draw.rect(screen, "bisque4", inner_bg, 0, 10) 
+    
+    # draw side background (light-brown)
     pygame.draw.rect(screen, "burlywood4", side_bg, 0, 10)
     pygame.draw.rect(screen, "black", inner_bg, 5, 10)
     
+    # draw vertical grid lines
     pygame.draw.line(screen, "black", (162, 40), (162, 558), 5)
     pygame.draw.line(screen, "black", (288, 40), (288, 558), 5)
     pygame.draw.line(screen, "black", (416, 40), (416, 558), 5)
     pygame.draw.line(screen, "black", (544, 40), (544, 558), 5)
 
+    # draw inner background/side background boundary 
     pygame.draw.line(screen, (100, 70, 40), (552, 40), (552, 560), 11)
     pygame.draw.line(screen, "black", (560, 40), (560, 558), 5)
-    
+
+# initialize rect, color, & text for "spin" button
 spin_button = pygame.Rect(574, 458, 170, 85)
 spin_button_color = (214, 207, 180)
 spin_font = pygame.font.Font(None, 75)
 spin_text = spin_font.render("SPIN", False, "black") 
 
+# initialize rect, color, & text for "x" button
 x_button = pygame.Rect(705, 54, 40, 40)
 x_button_color = (220, 20, 60)
 x_font = pygame.font.Font(None, 45)
 x_text = x_font.render("x", False, "black")
 
+# draw the game buttons
 def draw_buttons():
+    # draw spin button (light-tan)
     pygame.draw.rect(screen, spin_button_color, spin_button, 0, 30)
     pygame.draw.rect(screen, "black", spin_button, 5, 30)
     screen.blit(spin_text, (598, 479))
     
+    # draw x button (red)
     pygame.draw.rect(screen, x_button_color, x_button, 0, 50)
     pygame.draw.rect(screen, "black", x_button, 5, 50)
     screen.blit(x_text, (717, 58))
-    
+
+# draw all shapes on the grid polymorphically
 def fill_board(shape_list):
     for shape in shape_list:
         shape.draw_shape(screen)
         
+# print attributes of all shapes polymorphically
 def print_shapes(shape_list):
     for shape in shape_list:
         shape.print_shape()
-        
-def check_row_col(y_pos, shape_list):
-    row_shapes = [shape for shape in shape_list if shape.y == y_pos]
-    row_shapes = sorted(row_shapes, key=lambda shape: shape.x)
+
+# return list of adjacent shapes in row/col that have same type or color
+def check_row_col(pos, type, shape_list):
+    # if we want to check a row, grab all 4 shapes in a specific row 
+    if type == "row":
+        row_col_shapes = [shape for shape in shape_list if shape.y == pos]
+        row_col_shapes = sorted(row_col_shapes, key=lambda shape: shape.x)
+    # if we want to check a col, grab all 4 shapes in a specific col
+    else:
+        row_col_shapes = [shape for shape in shape_list if shape.x == pos]
+        row_col_shapes = sorted(row_col_shapes, key=lambda shape: shape.y)
     
-    max_adjacent_shapes = []
-    cur_adjacent_shapes = [row_shapes[0]]
+    max_matching_shapes = []
+    cur_matching_shapes = [row_col_shapes[0]]
     
-    for i in range(len(row_shapes) - 1):
-        if (row_shapes[i].type == row_shapes[i + 1].type) or \
-           (row_shapes[i].color == row_shapes[i + 1].color):
-            cur_adjacent_shapes.append(row_shapes[i + 1])
+    # iterate through the 4 shapes in the row or col 
+    for i in range(len(row_col_shapes) - 1):
+        # if the cur shape matches the next, add it to the cur matches list
+        if (row_col_shapes[i].type == row_col_shapes[i + 1].type) or \
+           (row_col_shapes[i].color == row_col_shapes[i + 1].color):
+            cur_matching_shapes.append(row_col_shapes[i + 1])
+        # if they do not match, update max and increment cur matches list
         else:
-            if len(cur_adjacent_shapes) > len(max_adjacent_shapes):
-                max_adjacent_shapes = cur_adjacent_shapes
-            cur_adjacent_shapes = [row_shapes[i + 1]]
+            if len(cur_matching_shapes) > len(max_matching_shapes):
+                max_matching_shapes = cur_matching_shapes
+            cur_matching_shapes = [row_col_shapes[i + 1]]
     
-    if len(cur_adjacent_shapes) > len(max_adjacent_shapes):
-        max_adjacent_shapes = cur_adjacent_shapes
-    if len(max_adjacent_shapes) == 1:
+    # check the last pair of adjacent shapes
+    if len(cur_matching_shapes) > len(max_matching_shapes):
+        max_matching_shapes = cur_matching_shapes
+        
+    # if there are no matching shapes, return empty list
+    if len(max_matching_shapes) == 1:
         return []
-    return max_adjacent_shapes
+    
+    return max_matching_shapes
 
 def draw_line(matching_shapes):
     length = len(matching_shapes)
@@ -122,71 +159,64 @@ def draw_line(matching_shapes):
              matching_shapes[0].y + 95 // 2),
             (matching_shapes[length - 1].x + 95 // 2, 
              matching_shapes[length - 1].y + 95 // 2),
-            10
+            5
         )
-        pygame.draw.line(
-            screen,
-            "white",
-            (matching_shapes[0].x + 95 // 2, 
-             matching_shapes[0].y + 95 // 2),
-            (matching_shapes[length - 1].x + 95 // 2, 
-             matching_shapes[length - 1].y + 95 // 2),
-            3
-        )
-        
     
 def check_board(shape_list):
-    first_row_matches = check_row_col(73, shape_list)
+    first_row_matches = check_row_col(73, "row", shape_list)
     draw_line(first_row_matches)
-    
-    second_row_matches = check_row_col(193, shape_list)
+    second_row_matches = check_row_col(193, "row", shape_list)
     draw_line(second_row_matches)
-    
-    third_row_matches = check_row_col(313, shape_list)
+    third_row_matches = check_row_col(313, "row", shape_list)
     draw_line(third_row_matches)
-    
-    fourth_row_matches = check_row_col(433, shape_list)
+    fourth_row_matches = check_row_col(433, "row", shape_list)
     draw_line(fourth_row_matches)
-
-
-
     
-        
-        
-        
-all_shapes = generate_shapes()
-
+    first_col_matches = check_row_col(55, "col", shape_list)
+    draw_line(first_col_matches)
+    second_col_matches = check_row_col(178, "col", shape_list)
+    draw_line(second_col_matches)
+    third_col_matches = check_row_col(305, "col", shape_list)
+    draw_line(third_col_matches)
+    fourth_col_matches = check_row_col(434, "col", shape_list)
+    draw_line(fourth_col_matches)
 
 while True:
-    draw_background()
+    # draw the game every frame
+    draw_background() 
+    draw_buttons()
     fill_board(all_shapes)
     check_board(all_shapes)
-    draw_buttons()
     
-
-    mouse_pos = pygame.mouse.get_pos()
+    mouse_pos = pygame.mouse.get_pos() # get mouse position every frame
     
+    # if the mouse position collides with the "spin" button, change its color
     if spin_button.collidepoint(mouse_pos):
         spin_button_color = (184, 177, 150)
     else:
         spin_button_color = (214, 207, 180)
+    # if the mouse position collides with the "x" button, change its color
     if x_button.collidepoint(mouse_pos):
         x_button_color = (190, 20, 30)
     else:
         x_button_color = (220, 20, 60)
-        
+    
+    # iterate through user input every frame
     for event in pygame.event.get():
+        # if the user closes the game window, quit the game
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
+            # if the user clicks the "spin" button, regenerate random shapes
             if event.button == 1 and spin_button.collidepoint(mouse_pos):
                 all_shapes = generate_shapes()
+            # if the user clicks the "x" button, quit the game
             if event.button == 1 and x_button.collidepoint(mouse_pos):
                 pygame.quit()
                 exit()
-                
     
+    # update the display every frame
     pygame.display.update()
     clock.tick(60)
 
--- shapes.py
+++ shapes.py
@@ -1,63 +1,86 @@
 import pygame
 import random
 
+# a random color & shape is chosen from these tuples when creating a shape
 colors = ("brown1", "darkolivegreen3", "cadetblue2", "gold1")
 shapes = ("square", "triangle", "circle", "diamond")
 
+# each point represents a position to place a shape (top-left)
 points = [
-    (55, 73), (178, 73), (305, 73), (434, 73),
+    (55, 73),  (178, 73),  (305, 73),  (434, 73),
     (55, 193), (178, 193), (305, 193), (434, 193),
     (55, 313), (178, 313), (305, 313), (434, 313),
     (55, 433), (178, 433), (305, 433), (434, 433)
 ]
 
+# return a random shape type and color
+def get_random_shape():
+    shape_type = random.choice(shapes)
+    color = random.choice(colors)
+    return shape_type, color
+
+# superclass to square, circle, triangle, & diamond subclasses
 class Shape:
+    # set the color & position of the shape
     def __init__(self, color, x, y):
         self.color = color
         self.x = x
         self.y = y
-    
+
+    # abstract method to be implemented by subclasses
     def draw_shape(self):
         raise NotImplementedError("Subclasses must implement draw_shape method")
-    
+
+    # abstract method to be implemented by subclasses
     def print_shape(self):
         raise NotImplementedError("Subclasses must implement print_shape method")
-    
+
+# subclass for square shape 
 class Square(Shape):
+    # set the color, position, size, and type of the square
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.size = 95
         self.type = "square"
     
+    # draw the square at its designated position
     def draw_shape(self, screen):
         rect = pygame.Rect(self.x, self.y, self.size, self.size)
         pygame.draw.rect(screen, self.color, rect)
         pygame.draw.rect(screen, "black", rect, 5)
         
+    # print the attributes of the square 
     def print_shape(self):
-        print(f"\{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")
+        print(f"\n{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")
 
+# subclass for the circle shape
 class Circle(Shape):
+    # set the color, position, size, and type of the circle
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.size = 48
         self.type = "circle"
-        
+    
+    # draw the circle at its designated position
     def draw_shape(self, screen):
         pygame.draw.circle(screen, self.color, 
                            (self.x + self.size, self.y + self.size), self.size)
         pygame.draw.circle(screen, "black", 
                            (self.x + self.size, self.y + self.size), self.size, 5)
-        
+    
+    # print the attributes of the circle 
     def print_shape(self):
         print(f"\n{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")
-        
+
+# subclass for the triangle shape
 class Triangle(Shape):
+    # set the color, position, size, and type of the triangle
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.size = 95
         self.type = "triangle"
 
+    # draw the triangle at its designated position 
     def draw_shape(self, screen):
         center_x = self.x + self.size // 2   
         center_y = self.y + self.size // 2  
@@ -70,16 +93,20 @@ class Triangle(Shape):
         
         pygame.draw.polygon(screen, self.color, triangle_points)
         pygame.draw.polygon(screen, "black", triangle_points, 5)
-        
+    
+    # print the attributes of the triangle
     def print_shape(self):
         print(f"\n{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")
-        
+
+# subclass for the diamond shape
 class Diamond(Shape):
+    # set the color, position, size, and type of the diamond
     def __init__(self, color, x, y):
         super().__init__(color, x, y)
         self.size = 95
         self.type = "diamond"
 
+    # draw the diamond at its designated position
     def draw_shape(self, screen):
         center_x = self.x + self.size // 2
         center_y = self.y + self.size // 2
@@ -94,10 +121,6 @@ class Diamond(Shape):
         pygame.draw.polygon(screen, self.color, diamond_points)
         pygame.draw.polygon(screen, "black", diamond_points, 7)
         
+    # print the attributes of the diamond
     def print_shape(self):
-        print(f"\n{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")
-        
-def get_random_shape():
-    shape_type = random.choice(shapes)
-    color = random.choice(colors)
-    return shape_type, color
+        print(f"\n{self.type}, {self.color}, X: {self.x}, Y: {self.y}\n")  
